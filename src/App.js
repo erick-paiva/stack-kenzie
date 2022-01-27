268f615c55318c881011e3925670c8355f234ce3
ErickDevMaster <123skatesk8@gmail.com>
2022-01-27 13:54:39 -0400

    feat: finished chakra input component and chakra text area

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import CardDoubts from "./components/CardDoubts";
+import { InputChakra } from "./components/InputChakra";
 // import ExampleContext from "./pages/ExampleContext"
 function App() {
-  return <div><CardDoubts /></div>;
+  return <div><CardDoubts />
+  
+  <InputChakra error={{message: "digite seu nome completo"}} label="adcione seu nome" placeholder="digite seu nome" />
+  </div>;
 }
 
 export default App;
---
+++ src/components/TextAreaChakra/index.jsx
@@ -0,0 +1,82 @@
+import {
+    FormControl,
+    FormErrorMessage,
+    FormLabel,
+    Input as ChakraInput,
+    InputLeftElement,
+    InputGroup,
+    ChakraProvider,
+  } from "@chakra-ui/react";
+  import { useState, useCallback, useEffect } from "react";
+  
+  const inputVariation = {
+    error: "red.500",
+    default: "gray.200",
+    focus: "purple.800",
+    filled: "green.500",
+  };
+  
+  export const TextAreaChakra = (
+    { name, label, icon: Icon, error = null, ...rest }
+  ) => {
+    const [value, setValue] = useState("");
+    const [variation, setVariation] = useState("default");
+  
+    useEffect(() => {
+      if (error) {
+        return setVariation("error");
+      }
+    }, [error]);
+  
+    const handleInputFocus = useCallback(() => {
+      if (!error) {
+        setVariation("focus");
+      }
+    }, [error]);
+  
+    const handleInputBlur = useCallback(() => {
+      if (value.length > 1 && !error) {
+        return setVariation("filled");
+      }
+    }, [error, value]);
+  
+    return (
+        <FormControl isInvalid={!!error}>
+          {!!label && <FormLabel color="gray.400">{label}</FormLabel>}
+  
+          <InputGroup flexDirection="column">
+            {Icon && (
+              <InputLeftElement color={inputVariation[variation]} mt="2.5">
+                <Icon />
+              </InputLeftElement>
+            )}
+  
+            <TextAreaChakra
+              id={name}
+              name={name}
+              onChangeCapture={(e) => setValue(e.currentTarget.value)}
+              onBlurCapture={handleInputBlur}
+              onFocus={handleInputFocus}
+              borderColor={inputVariation[variation]}
+              color={inputVariation[variation]}
+              bg="gray.50"
+              variant="outline"
+              _hover={{ bgColor: "gray.100" }}
+              _placeholder={{ color: "gray.300" }}
+              _focus={{
+                bg: "gray.100",
+              }}
+              size="lg"
+              h="60px"
+              
+              {...rest}
+            />
+  
+            {!!error && (
+              <FormErrorMessage color="red.500">{error.message}</FormErrorMessage>
+            )}
+          </InputGroup>
+        </FormControl>
+    );
+  };
+  
