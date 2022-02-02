5ec5622f2fa06e2b8287c3c78271e39d7ce6dac0
Julio Cezar Pereira <juliocezarpereira@Julios-MacBook.lan>
2022-02-02 10:30:18 -0300

    fix: update providers

--- src/providers/hooks/index.jsx
+++ src/providers/hooks/index.jsx
@@ -1,12 +1,25 @@
 import { useContext } from "react";
 import { AuthContext } from "../contexts/auth";
+//import { UserContext } from "../contexts/users";
 import { QuestionContext } from "../contexts/questions";
 import { AnswerContext } from "../contexts/answers";
 import { CommentContext } from "../contexts/comments";
+import { TagContext } from "../contexts/tags";
 
 //Hook useAuth
 export const useAuth = () => {
   const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+
+  return context;
+};
+
+//Hook useAuth
+export const useUsers = () => {
+  const context = useContext();
 
   if (!context) {
     throw new Error("useAuth must be used within an AuthProvider");
@@ -26,6 +39,17 @@ export const useQuestions = () => {
   return context;
 };
 
+//Hook useAnswers
+export const useAnswers = () => {
+  const context = useContext(AnswerContext);
+
+  if (!context) {
+    throw new Error("useQuestion must be used within an AuthProvider");
+  }
+
+  return context;
+};
+
 //Hook useComments
 export const useComments = () => {
   const context = useContext(CommentContext);
@@ -37,9 +61,9 @@ export const useComments = () => {
   return context;
 };
 
-//Hook useAnswers
-export const useAnswers = () => {
-  const context = useContext(AnswerContext);
+//Hook useComments
+export const useTags = () => {
+  const context = useContext(TagContext);
 
   if (!context) {
     throw new Error("useQuestion must be used within an AuthProvider");
