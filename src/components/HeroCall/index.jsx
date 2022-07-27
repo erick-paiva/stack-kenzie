a169d3d60cab3528b721667e14c17e77e5983241
rhamo1 <rhamo1@outlook.com>
2022-07-27 16:00:01 -0300

    fix: herocall text.

--- src/components/HeroCall/index.jsx
+++ src/components/HeroCall/index.jsx
@@ -9,30 +9,22 @@ export default function HeroCall() {
       <VStack pos="relative" spacing={"40px"}>
         <Flex alignItems={"center"} mt={"50px"}>
           <BlueDot />
-          <Text w="100%">
+          <Text w="80%">
             Faça perguntas aos coachs da Kenzie Academy Brasil
           </Text>
         </Flex>
         <Flex alignItems={"center"}>
           <BlueDot />
-          <Text w="100%">
+          <Text w="80%">
             Comente as postagens dos colegas e ajude a tirar dúvidas
           </Text>{" "}
         </Flex>
         <Flex alignItems={"center"}>
           <BlueDot />
-          <Text w="100%">
-            Comente as postagens dos colegas e ajude a tirar dúvidas
+          <Text w="80%">
+            Curta as dúvidas mais interessantes para colocar em evidência{" "}
           </Text>{" "}
         </Flex>
-        <Box
-          h={"160px"}
-          w={"5px"}
-          bg={"primary"}
-          position={"absolute"}
-          right="295px"
-          top={"40px"}
-        />
       </VStack>
     </Box>
   );
